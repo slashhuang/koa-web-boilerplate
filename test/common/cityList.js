5ca45d0dedd74be4f5e39e6ef01a296862e8f74b
slashhuang <627284482@qq.com>
2017-01-10 11:32:51 +0800

    update unit test

--- test/common/cityList.js
+++ test/common/cityList.js
@@ -9,12 +9,58 @@ import {
     S_estateList} from '../../app/services/index.js';
 
 describe('cityList', function() {
-    it('should work', function(done) {
+    it('/common/cityList.action with args 2 should work', function(done) {
         (async ()=>{
             let cityList =   await S_cityList({
                 provinceId:2
             });
-            console.log(cityList)
+            console.log(cityList);
+            done()
+        })()
+    });
+});
+describe('cityList', function() {
+    it('/common/cityList.action without args should work', function(done) {
+        (async ()=>{
+            let cityList =   await S_cityList({
+                provinceId:''
+            });
+            console.log(cityList);
+            done()
+        })()
+    });
+});
+describe('townList', function() {
+    it('/common/townList.action with args cityId should work', function(done) {
+        (async ()=>{
+            let townList =   await S_townList({
+                cityId:1
+            });
+            console.log(townList);
+            done()
+        })()
+    });
+});
+describe('townList', function() {
+    it('/common/townList.action without cityId should work', function(done) {
+        (async ()=>{
+            let townList =   await S_townList({
+                cityId:''
+            });
+            if(townList){
+                console.log(townList['data']);
+                done()
+            }
+        })()
+    });
+});
+describe('estateList', function() {
+    it('/common/estateList.action should work', function(done) {
+        (async ()=>{
+            let estateList = await S_estateList({
+                keyword:'虹桥'
+            });
+            console.log(estateList);
             done()
         })()
     });
