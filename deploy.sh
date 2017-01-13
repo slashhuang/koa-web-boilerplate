b4a76b8232481c0131cedd7d050a097affd67ae5
Enzo <enzoyang@superjia.com>
2017-01-13 13:17:45 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t iwjw-rent-platform-im:latest .
-docker ps -a -q -f name=iwjw-rent-platform | xargs -I {} docker stop {}
-docker ps -a -q -f name=iwjw-rent-platform | xargs -I {} docker rm {}
+docker stop iwjw-rent-platform
+docker rm iwjw-rent-platform
 docker run -d -p 7000:7000 --name iwjw-rent-platform iwjw-rent-platform-im:latest
