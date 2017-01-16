f52d946389bcb3c62ddff8f26ede993f7ad84006
slashhuang <627284482@qq.com>
2017-01-16 19:21:13 +0800

    updatinig config

---
+++ config/conf/beta.config.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: slashhuang
+ */
+
+import {checkoutENV} from './util.js';
+
+module.exports = {
+    /**
+     * 静态资源配置
+     */
+
+    staticConfigs: checkoutENV(),
+
+    rentSOA:'http://rentsoa.iwjwbeta.com',
+
+    //rentSOA:'http://10.7.251.73:9090',
+    //proxyTarget:'http://10.7.251.73:9090',
+    //proxyTarget:"http://192.168.1.35:1313",
+
+    //图片上传转发服务器
+    proxyTarget:'http://rentsoa.iwjwbeta.com',
+
+    website: 'http://192.168.1.121:7000'
+};
