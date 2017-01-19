42785cc7cda724093dc90847b2e099bd94f107da
slashhuang <627284482@qq.com>
2017-01-19 20:55:23 +0800

    add prod.config

--- config/conf/prod.config.js
+++ config/conf/prod.config.js
@@ -15,6 +15,14 @@ module.exports = {
         //开发环境配置选项
         devResourceURL: `http://127.0.0.1/${projectName}/`
     },
+    rentSOA:'http://100.114.148.239/',
+
+    //rentSOA:'http://10.7.251.73:9090',
+    //proxyTarget:'http://10.7.251.73:9090',
+    //proxyTarget:"http://192.168.1.35:1313",
+
+    //图片上传转发服务器
+    proxyTarget:'http://100.114.148.239/',
     // 应用端口
     port: 80,
 
