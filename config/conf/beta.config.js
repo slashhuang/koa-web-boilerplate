b10dcfdf0ce31cb9d105fe331a3d3fe2798bf857
slashhuang <627284482@qq.com>
2017-01-19 18:03:32 +0800

    reivisng

--- config/conf/beta.config.js
+++ config/conf/beta.config.js
@@ -14,14 +14,14 @@ module.exports = {
         devResourceURL: `http://127.0.0.1/${projectName}/`
     },
 
-    rentSOA:'http://rentsoa.iwjwbeta.com',
+    rentSOA:'http://10.28.28.157:8611/',
 
     //rentSOA:'http://10.7.251.73:9090',
     //proxyTarget:'http://10.7.251.73:9090',
     //proxyTarget:"http://192.168.1.35:1313",
 
     //图片上传转发服务器
-    proxyTarget:'http://rentsoa.iwjwbeta.com',
+    proxyTarget:'http://10.28.28.157:8611/',
 
     website: 'http://118.178.242.96:7000'
 };
