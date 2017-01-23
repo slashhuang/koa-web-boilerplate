f79775a97dc4259baac280de7be3e841edbd951e
slashhuang <627284482@qq.com>
2017-01-23 16:32:04 +0800

    remove company based deploy docker files

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker build -t iwjw-rent-platform-im:latest .
-docker stop iwjw-rent-platform
-docker rm iwjw-rent-platform
-docker run -d -p 7000:7000 --name iwjw-rent-platform iwjw-rent-platform-im:latest
